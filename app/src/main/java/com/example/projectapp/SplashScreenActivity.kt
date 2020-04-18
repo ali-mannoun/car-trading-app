b8a35b147d0e47ff6c7ebb3e74f74115e179c493
Ali-101 <ali.mannoun.1@gmail.com>
2020-04-18 22:13:25 +0300

    Add splash screen

---
+++ app/src/main/java/com/example/projectapp/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.projectapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+
+class SplashScreenActivity : AppCompatActivity() {
+    val SPLASH_TIME: Long = 2000; //This is 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Log.e("Splash","ssssssssssss")
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        //Code to start timer and take action after the timer ends
+        Handler().postDelayed(Runnable {
+            //Do any action here. Now we are moving to next page
+            val mySuperIntent = Intent(this, MasterActivity::class.java)
+            startActivity(mySuperIntent);
+
+            //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+            finish();
+        }, SPLASH_TIME);
+    }
+}
